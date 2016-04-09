c8bf948540f6b79d10421e357075acb1ade1620a
Lawrence Pan <lawrencefeipan@gmail.com>
2016-04-09 13:15:17 -0400

    completed part 1, now index.php responds to get request

--- public/index.php
+++ public/index.php
@@ -1,8 +1,28 @@
 <?php
-require "../vendor/autoload.php";
+require '../vendor/autoload.php';
 
+function _getDate($filename="") {
+    if ($filename) {
+        return date('D, d M Y H:i:s T', filemtime($filename));
+    } else {
+        return date('D, d M Y H:i:s T');
+    }
+}
+
+
+$response = array(
+    'to' => 'Pillr',
+    'subject' => 'Hello Pillr',
+    'message' => 'Here is my submission.',
+    'from' => 'Lawrence Pan',
+    'timeSent' => (string)time(),
+);
+
+header('Date: ' . _getDate());
+header('Last-Modified: ' . _getDate(__FILE__));
+header('Content-Type: application/json');
+
+
+echo json_encode($response);
 # TIP: Use the $_SERVER Sugerglobal to get all the data your need from the Client's HTTP Request.
-
-# TIP: HTTP headers are printed natively in PHP by invoking header().
-#      Ex. header('Content-Type', 'text/html');
 ?>
